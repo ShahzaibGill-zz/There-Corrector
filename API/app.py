74a1f245be9e745c0e4ba300ae9c867ca59c25ad
Shahzaib Gill <shahzaibgill@gmail.com>
2016-10-01 17:21:48 -0400

    Added comments

--- API/app.py
+++ API/app.py
@@ -11,24 +11,27 @@ def index():
 @app.route('/<string:text>')
 def stringCorrector(text):
     inputText = nlp.word_tokenize(text)
-    text_array = nlp.pos_tag(inputText)
+    # Tags each word with part of speech tags. Refer to Penn Treebank Project for more information
+    taggedText = nlp.pos_tag(inputText)
     for index, word in enumerate(inputText):
-        if word in ('There', 'there', 'Their', 'their', '\'re'):
-            if text_array[index + 1][1] in ('VB', 'VBD', 'VBN', 'VBP', 'VBZ', 'MD'):
-
-                if index == 0 or index - 1 >= 0 and inputText[index - 1] == '.':
+        if word in ('There', 'there', 'Their', 'their') or (word == '\'re' and inputText[index - 1] in ('they', 'They')):
+            # Since "They're" is split between two indexes, delete previous index so corrected word is not appended to "They"
+            if word == '\'re' and inputText[index - 1] in ('they', 'They'):
+                del inputText[index - 1]
+                del taggedText[index -1]
+                index = index - 1
+            if taggedText[index + 1][1] in ('VB', 'VBD', 'VBN', 'VBP', 'VBZ', 'MD'):
+                if index == 0 or (index - 1 >= 0 and inputText[index - 1] == '.'):
                     inputText[index] = 'There'
                 else:
                     inputText[index] = 'there'
-            elif text_array[index + 1][1] in ('NN', 'NNS', 'NNP', 'NNPS'):
-                if index == 0 or index - 1 >= 0 and inputText[index - 1] == '.':
+            elif taggedText[index + 1][1] in ('NN', 'NNS', 'NNP', 'NNPS'):
+                if index == 0 or (index - 1 >= 0 and inputText[index - 1] == '.'):
                     inputText[index] = 'Their'
                 else:
                     inputText[index] = 'their'
-            elif text_array[index + 1][1] in ('IN', 'JJ', 'VBG'):
-                if inputText[index - 1] in ('they', 'They'):
-                    inputText[index - 1] = ''
-                if index == 0 or index - 1 >= 0 and inputText[index - 1] == '.':
+            elif taggedText[index + 1][1] in ('IN', 'JJ', 'VBG', 'RB','RBR','RBS'):
+                if index == 0 or (index - 1 >= 0 and inputText[index - 1] == '.'):
                     inputText[index] = 'They\'re'
                 else:
                     inputText[index] = 'they\'re'
