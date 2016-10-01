1939693168ff5b54997bffd7779acb2b01657b4e
Shahzaib Gill <shahzaibgill@gmail.com>
2016-10-01 17:16:28 -0400

    Fixed they're condition

--- Chrome Extension/popup.js
+++ Chrome Extension/popup.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
   var link = document.getElementById('submitbutton');
   link.addEventListener('click', function() {
-        text = document.getElementById('inputbox').value;
+        text = document.getElementById('inputBox').value;
         var xhr = new XMLHttpRequest();
         xhr.open("GET", "http://there-corrector.herokuapp.com/"+text, false);
         xhr.send();
         if (xhr.status == '200'){
-          document.getElementById('inputbox').value= xhr.responseText;
+          document.getElementById('inputBox').value= xhr.responseText;
           console.log(xhr.statusText);
         }
         else {
-          document.getElementById('inputbox').value= 'An error occured. Please try again.';
+          document.getElementById('inputBox').value= 'An error occured. Please try again.';
         }
     });
 });
